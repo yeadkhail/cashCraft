7bd65feb11e81cfea764697f161d4e85931a3c80
GitGud9786 <tausif1250@gmail.com>
2024-03-14 02:11:09 +0600

    Added log-in interface
    This commit adds the feature to log into the application via password.

---
+++ src/main/java/com/example/cashcraft/ExceptionCatcher.java
@@ -0,0 +1,6 @@
+package com.example.cashcraft;
+
+public class ExceptionCatcher extends Exception
+{
+    public ExceptionCatcher(String message){super(message);}
+}
---
+++ src/main/java/com/example/cashcraft/WindowSwitcher.java
@@ -0,0 +1,57 @@
+package com.example.cashcraft;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.sql.*;
+
+public class WindowSwitcher
+{
+    @FXML
+    private TextField passfield;
+    private Scene scene;
+    private Stage stage;
+    private Parent root;
+    public void log_in(ActionEvent event)throws IOException
+    {
+        try
+        {
+            if(passfield.getText().isEmpty()) {throw new ExceptionCatcher("Invalid input!");}
+            else
+            {
+                String pass="";
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+                Statement statement = connection.createStatement();
+                ResultSet rs=statement.executeQuery("select password from user where id=701");
+                if(rs.next()) {pass = rs.getString("password");}
+                if(passfield.getText().equals(pass))
+                {
+                    root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+                    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+                else throw new ExceptionCatcher("Wrong input!");
+            }
+        } catch(ExceptionCatcher ex)
+        {
+            passfield.clear();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Input is EMPTY!");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+        } catch (SQLException e) {
+            passfield.clear();
+            throw new RuntimeException(e);
+        }
+    }
+}
