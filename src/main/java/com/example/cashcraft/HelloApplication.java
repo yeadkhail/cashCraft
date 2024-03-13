98b4c040c57a0d2bd010d1889540cd36b466dcf7
yeadkhail <119938455+yeadkhail@users.noreply.github.com>
2024-03-14 00:43:41 +0600

    Added documentation comments to the HelloApplication.java file and added the link for the view-only E-R diagram
    Added the documentation comments for the HelloApplication.java file and added the documentation of main method of the application.
    The view only E-R diagram of the database structure is also included in the comments.

--- src/main/java/com/example/cashcraft/HelloApplication.java
+++ src/main/java/com/example/cashcraft/HelloApplication.java
@@ -1,3 +1,12 @@
+/**
+ * This hello-application.java file implements the application of cashCraft.
+ * It holds the main function for the project and holds the application class from javafx application
+ * @ author: Md. Yead Khail Shovo , Muhammad Tausif Ul Islam , Shahir Awlad
+ * The view-only link for the E-R diagram for the sqlite-database : https://lucid.app/lucidchart/95af82fb-b0e0-4918-9560-080446c1473c/edit?viewport_loc=-602%2C-186%2C2834%2C1218%2C0_0&invitationId=inv_66e7b2eb-a3c9-4cc4-be92-b9020dd5d2c9
+ * Database structure is changed only on request-only basis.
+ *
+ */
+
 package com.example.cashcraft;
 
 import javafx.application.Application;
@@ -12,11 +21,15 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("CashCraft");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * This is the main method for the application
+     * @param args not used
+     */
     public static void main(String[] args) {
         launch();
     }
